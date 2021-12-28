c2a87c62aef51767dac7e03efca0125d80ee1667
memayvtc001 <hoangcmgcs190814@fpt.edu.vn>
2021-12-28 09:21:30 +0700

    Add project files.

---
+++ AM2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AM2.MvcApplication" Language="C#" %>
---
+++ AM2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AM2.Startup))]
+namespace AM2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
